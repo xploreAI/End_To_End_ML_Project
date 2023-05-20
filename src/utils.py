00f46d419c14b26acfde447063c4ae12e3f25a43
xplorea1 <102108958+xplorea1@users.noreply.github.com>
2023-05-20 17:35:58 +0100

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -50,6 +51,13 @@ def evaluate_models(X_train, X_test, y_train, y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
